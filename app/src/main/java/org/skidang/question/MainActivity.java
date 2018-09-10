ad4d881d32d8af241371aeb727618ae103c8b39d
ggcc73wjk <ggcc73wjk@hcdd.kr>
2018-09-10 10:41:31 +0900

    02-5 완료

--- app/src/main/java/org/skidang/question/MainActivity.java
+++ app/src/main/java/org/skidang/question/MainActivity.java
@@ -1,13 +1,56 @@
 package org.skidang.question;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    ScrollView scollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 레이아웃에 정의된 뷰 객체 참조
+        scollView = (ScrollView) findViewById(R.id.scollView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        // 수평 스크롤바 사용 기능 설정
+        scollView.setHorizontalScrollBarEnabled(true);
+
+        // 리소스의 이미지 참조
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.arsenal);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        // 이미지 리소스와 이미지 크기 설정
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
+
+    public void changeImage(){
+        // 리소스의 이미지 참조
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.cup2);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        // 이미지 리소스와 이미지 크기 설정
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
     }
 }
