927b3f538b0040be1dcca0770ee72a96c0f490dc
ggcc73wjk <ggcc73wjk@hcdd.kr>
2018-09-10 11:10:20 +0900

    02_6 완료

--- app/src/main/java/org/skidang/question/MainActivity.java
+++ app/src/main/java/org/skidang/question/MainActivity.java
@@ -1,13 +1,40 @@
 package org.skidang.question;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+    int imageIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+    }
+
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
+
+    public void changeImage(){
+        if(imageIndex == 0){
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex = 1;
+        }else if(imageIndex == 1){
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+            imageIndex = 0;
+        }
     }
 }
