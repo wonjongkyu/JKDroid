e79f2a9cc2b5f91392d37f65c74219615cb9aee9
ggcc73wjk <ggcc73wjk@hcdd.kr>
2018-11-29 10:37:46 +0900

    no message

---
+++ app/src/main/java/org/skidang/question/LoginActivity.java
@@ -0,0 +1,20 @@
+package org.skidang.question;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void onClicked1(View v){
+
+
+    }
+}
